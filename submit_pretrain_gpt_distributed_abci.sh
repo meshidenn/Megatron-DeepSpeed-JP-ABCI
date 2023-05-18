d09b819b6dfc6c23fb9f5e56a5cb61976c892e70
idaten459 <shukai2013@gmail.com>
2023-05-19 08:40:44 +0900

    verify

---
+++ submit_pretrain_gpt_distributed_abci.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#$ -l rt_F=1 # 1 node, 4 gpu
+#$ -l h_rt=0:30:00 # 30 minutes
+#$ -j y # Merge standard error output into standard output.
+#$ -cwd
+
+WORK_DIR={your working directory}
+
+source /etc/profile.d/modules.sh
+module load python/3.10/3.10.10 cuda/11.8/11.8.0 cudnn/8.6/8.6.0 nccl/2.16/2.16.2-1
+source $WORK_DIR/megatron-deepspeed-abci/bin/activate
+cd $WORK_DIR/Megatron-DeepSpeed-Ylab
+
+sh examples/pretrain_gpt_distributed_abci.sh
